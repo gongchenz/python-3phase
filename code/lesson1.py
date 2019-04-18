13794937ef80a9173a3cf5e2a25f8b18cb8d48c8
gongchenz <gongchenzhang@qq.com>
2019-04-18 17:44:48 +0800

    这个是第一课的代码

---
+++ code/lesson1.py
@@ -0,0 +1,19 @@
+# 顺序
+print("今天天气很好")
+print("今天我想吃点好的")
+
+# 分支
+# m = 10
+# if 10>m:
+#     print("吃")
+# else:
+#     print("不吃")
+
+# 重复
+for day in range(20):
+    # 分支
+    m = day
+    if 10>m:
+        print("吃")
+    else:
+        print("不吃")
