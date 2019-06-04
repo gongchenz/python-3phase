6dfa34dd14af09ac9c29faed8747db24f4c6128b
gongchenz <gongchenzhang@qq.com>
2019-06-04 18:53:28 +0800

    增加第二课的内容

---
+++ code/lesson2-20190604.py
@@ -0,0 +1,16 @@
+# 列表
+a = ['a', 'b', 'c']
+print(a.count('a'))
+print('1:', a)
+del a[2]
+print('2:', a)
+
+# 元组 
+b = ('a', 'b', 'c')
+print('3:', b)
+#del b[2]
+print('4:', b)
+
+# 字典
+dic = {'name1':'zgc', 'name2':'xiaoming'}
+print(dic['name1'])
